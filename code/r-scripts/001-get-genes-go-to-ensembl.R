c02e1037db637222b6b4cd0199afd516467851d3
amir-rakhimov <amir.rakhimov.b@gmail.com>
2025-04-08 16:15:48 +0900

    added interpro data retrieval

--- code/r-scripts/001-get-genes-go-to-ensembl.R
+++ code/r-scripts/001-get-genes-go-to-ensembl.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 library(biomaRt)
+library(httr)
+library(jsonlite)
+source("code/r-scripts/check_interpro_hierarchy.R")
+
 #' ---
 #' title: "Obtaining Ensembl IDs and coordinates of genes associated with a 
 #' pathway based on its Gene ontology (GO) ID"
@@ -71,30 +75,32 @@ head(go_genes.table)
 # transcript_length Transcript length (including UTRs and CDS)
 
 my.attr.feature<-c("entrezgene_id",
-                         "chromosome_name",
-                         "start_position","end_position","strand",
-                         "ensembl_gene_id","ensembl_gene_id_version",
-                         "ensembl_transcript_id","ensembl_transcript_id_version",
-                         "external_gene_name","transcript_start",
-                         "transcript_end","transcription_start_site",
-                         "transcript_length","transcript_count","transcript_is_canonical")
+                   "chromosome_name",
+                   "start_position","end_position","strand",
+                   "ensembl_gene_id","ensembl_gene_id_version",
+                   "ensembl_transcript_id","ensembl_transcript_id_version",
+                   "external_gene_name","transcript_start",
+                   "transcript_end","transcription_start_site",
+                   "transcript_length","transcript_count","transcript_is_canonical")
+# InterPro start and end coordinates refer to amino acid sequence
+my.attr.feature.interpro<-c("ensembl_gene_id","ensembl_gene_id_version",
+                            "ensembl_transcript_id","ensembl_transcript_id_version",
+                            "transcript_is_canonical","external_gene_name",
+                            "interpro","interpro_short_description",
+                            "interpro_description","interpro_start","interpro_end")
 my.attr.structure<-c("ensembl_gene_id","ensembl_gene_id_version",
-                             "ensembl_transcript_id","ensembl_transcript_id_version",
-                             "exon_chrom_start","exon_chrom_end",
-                             "rank","cdna_coding_start",
-                             "cdna_coding_end",
-                             "cds_start","cds_end",
-                             "genomic_coding_start","genomic_coding_end",
-                             "5_utr_start","5_utr_end",
-                             "3_utr_start","3_utr_end","cds_length")
+                     "ensembl_transcript_id","ensembl_transcript_id_version",
+                     "exon_chrom_start","exon_chrom_end",
+                     "rank","cdna_coding_start",
+                     "cdna_coding_end",
+                     "cds_start","cds_end",
+                     "genomic_coding_start","genomic_coding_end",
+                     "5_utr_start","5_utr_end",
+                     "3_utr_start","3_utr_end","cds_length")
 my.attr.feature.mgi<-c("mgi_id","mgi_symbol",my.attr.feature)
-my.attr.feature.uniprot_gn<-c("uniprot_gn_id","mgi_symbol",my.attr.feature)
+my.attr.feature.uniprot_gn<-c("uniprot_gn_id","mgi_symbol","ensembl_gene_id")
 my.attr.feature.uniprot_iso<-c("mgi_symbol","uniprot_isoform",
-                                    my.attr.feature)
-# my.attr.mgi.structure<-c("mgi_id","mgi_symbol",my.attr.structure)
-# my.attr.uniprot_gn.structure<-c("uniprot_gn_id","mgi_symbol",my.attr.structure)
-# my.attr.uniprot_iso.structure<-c("mgi_symbol","uniprot_isoform",
-#                               my.attr.structure)
+                                    "ensembl_gene_id")
 
 #' Rename the columns according to their contents: MGI ID, gene product,
 #' gene symbol, source (database; almost all are from MGI), type (protein,
@@ -149,7 +155,9 @@ head(bm.feature.mgi)
 #' Here, filter based on `uniprot_isoform` because there's one isoform from the
 #' UniProt database. 
 #' The `mart` object is the `ensembl.ds` dataset that we got earlier.
-#' 
+#' First, get the Ensembl gene id because the isoform might point to a 
+#' non-canonical transcript (and we want canonical ones). 
+#' Then, find the other attributes based on ensembl gene ids.
 bm.feature.uniprot_iso<-getBM(
   attributes=my.attr.feature.uniprot_iso,
   filters = "uniprot_isoform",
@@ -157,10 +165,19 @@ bm.feature.uniprot_iso<-getBM(
   mart=ensembl.ds
 )
 head(bm.feature.uniprot_iso)
+# Here, we get the other attributes based on ensembl gene id.
+bm.feature.uniprot_iso<-getBM(
+  attributes=my.attr.feature,
+  filters = "ensembl_gene_id",
+  values=bm.feature.uniprot_iso$ensembl_gene_id,
+  mart=ensembl.ds
+)
+head(bm.feature.uniprot_iso)
 
 # Convert the chromosome name to character for joining in subsequent steps.
 bm.feature.uniprot_iso$chromosome_name<-
   as.character(bm.feature.uniprot_iso$chromosome_name)
+
 #' Find information for genes from the `non_mgi.genes` vector.
 #' Here, filter based on `uniprot_gn_id` because we have a gene from the UniProt 
 #' database.
@@ -170,6 +187,15 @@ bm.feature.uniprot_gn<-getBM(
   values=non_mgi.genes,
   mart=ensembl.ds
 )
+# Here, we get the other attributes based on ensembl gene id.
+bm.feature.uniprot_gn<-getBM(
+  attributes=my.attr.feature,
+  filters = "ensembl_gene_id",
+  values=bm.feature.uniprot_gn$ensembl_gene_id,
+  mart=ensembl.ds
+)
+head(bm.feature.uniprot_gn)
+
 # Convert the chromosome name to character for joining in subsequent steps.
 bm.feature.uniprot_gn$chromosome_name<-
   as.character(bm.feature.uniprot_gn$chromosome_name)
@@ -183,8 +209,9 @@ bm.feature.all<-bind_rows(bm.feature.mgi,
   dplyr::select(all_of(my.attr.feature.mgi))
 head(bm.feature.all)
 
-#' Get unique stable ensembl gene_ids that correspond to the combined dataset
-bm.ensembl_gene_id.all<-bm.feature.all%>%
+#' Get unique stable ensembl gene_ids that correspond to the combined feature dataset.
+#' We will update it later because we will add genes from the structure data.
+bm.feature.all.ensembl_gene_id<-bm.feature.all%>%
   distinct(ensembl_gene_id)%>%
   arrange%>%
   pull
@@ -194,13 +221,13 @@ bm.ensembl_gene_id.all<-bm.feature.all%>%
 bm.structure.all<-getBM(
   attributes=my.attr.structure,
   filters = "ensembl_gene_id",
-  values=bm.ensembl_gene_id.all,
+  values=bm.feature.all.ensembl_gene_id,
   mart=ensembl.ds)
 head(bm.structure.all)
 
 
 #' Bind two tables: keep only canonical ensembl transcripts
-bm.all<-bm.feature.all%>%
+bm.feature.structure.all<-bm.feature.all%>%
   full_join(bm.structure.all,
             by = join_by(ensembl_gene_id, 
                          ensembl_gene_id_version, 
@@ -211,32 +238,76 @@ bm.all<-bm.feature.all%>%
   rename("gene_name"="external_gene_name")
 
 # The mgi_id are the same, but mgi_symbol are different
-setdiff(bm.all$mgi_id,mgi.genes.table$mgi_id)
-setdiff(mgi.genes.table$mgi_id,bm.all$mgi_id)
-setdiff(bm.all$mgi_symbol,mgi.genes.table$gene_symbol)
-setdiff(mgi.genes.table$gene_symbol,bm.all$mgi_symbol)
+setdiff(bm.feature.structure.all$mgi_id,mgi.genes.table$mgi_id)
+setdiff(mgi.genes.table$mgi_id,bm.feature.structure.all$mgi_id)
+setdiff(bm.feature.structure.all$mgi_symbol,mgi.genes.table$gene_symbol)
+setdiff(mgi.genes.table$gene_symbol,bm.feature.structure.all$mgi_symbol)
 #' Remove duplicated IDs (Trim30b) 
-bm.all<-bm.all%>%
+bm.feature.structure.all<-bm.feature.structure.all%>%
   group_by(ensembl_transcript_id_version,mgi_id,exon_chrom_start)%>%
   distinct(ensembl_transcript_id_version,mgi_id,exon_chrom_start,.keep_all = TRUE)%>%
   ungroup()
 #' Verify that we didn't lose any MGI IDs or Ensembl genes
-stopifnot(length(unique(bm.all$mgi_id))==length(unique(mgi.genes.table$mgi_id)))
-stopifnot(length(unique(bm.all$ensembl_gene_id_version))==
+bm.feature.structure.all.mgi_id.uniq<-unique(bm.feature.structure.all$mgi_id)
+# Remove NA mgi_id for verification
+bm.feature.structure.all.mgi_id.uniq<-bm.feature.structure.all.mgi_id.uniq[!is.na(bm.feature.structure.all.mgi_id.uniq)]
+# Same for mgi.genes.table
+mgi.genes.table.mgi_id.uniq<-unique(mgi.genes.table$mgi_id)
+# We want the joined biomart table to have same amount of mgi id
+stopifnot(length(bm.feature.structure.all.mgi_id.uniq)==length(mgi.genes.table.mgi_id.uniq))
+# But it's possible that the joined biomart has more ensembl gene ids because
+# we are getting them from structure data (uniprot ids). So the length of two
+# vectors doesn't have to be the same: iomart table length should be more 
+# than or equal to the length of mgi id table
+stopifnot(length(unique(bm.feature.structure.all$ensembl_gene_id_version))>=
             length(unique(bm.feature.mgi$ensembl_gene_id_version)))
 
 
 # Separate coding and non-coding sequences (pseudogenes) into two tables
-bm.all.coding<-bm.all%>%
+bm.feature.structure.all.coding<-bm.feature.structure.all%>%
   group_by(ensembl_gene_id_version)%>%
   filter(cds_length==max(cds_length))%>%
   ungroup()
 
-bm.all.noncoding<-bm.all%>%
+bm.feature.structure.all.noncoding<-bm.feature.structure.all%>%
   group_by(ensembl_gene_id_version)%>%
   filter(is.na(cds_length))%>%
   ungroup()
 
+# Get InterPro domains. We can get them only from coding sequences (no pseudogenes!)
+# Extract the ensembl gene ids of coding sequences
+bm.feature.structure.all.coding.ensembl_gene_id<-bm.feature.structure.all.coding%>%
+  distinct(ensembl_gene_id)%>%
+  arrange%>%
+  pull
+# Get the protein domain data from biomart 
+bm.interpro.all<-getBM(
+  attributes=my.attr.feature.interpro,
+  filters = "ensembl_gene_id",
+  values=bm.feature.structure.all.coding.ensembl_gene_id,
+  mart=ensembl.ds)
+# Extract unique InterPro IDs
+bm.interpro.all.interpro_id<-bm.interpro.all%>%
+  filter(transcript_is_canonical==1)%>%
+  filter(interpro!="")%>%
+  distinct(interpro)%>%
+  pull
+# Identify families and homologous superfamilies
+interpro_ids.to.remove<-check_interpro_hierarchy(bm.interpro.all.interpro_id)
+# interpro_ids.to.remove.tryagain<-check_interpro_hierarchy(interpro_ids.to.remove[[2]])
+# Select canonical transcripts and remove families and superfamilies
+bm.interpro.all.clean<-bm.interpro.all%>%
+  filter(transcript_is_canonical==1)%>%
+  filter(interpro!="")
+  filter(!interpro%in%interpro_ids.to.remove[[1]])
+# We must not lose ensembl gene ids: all elements of feature+structure biomart dataset
+# should be found in the cleaned interpro dataset, and vice versa 
+# TODO: some proteins don't have any domains, only families annotation.
+stopifnot(length(setdiff(unique(bm.feature.structure.all.coding$ensembl_gene_id),
+                         unique(bm.interpro.all.clean$ensembl_gene_id)))==0) # This one particularly
+stopifnot(length(setdiff(unique(bm.interpro.all.clean$ensembl_gene_id),
+                         unique(bm.feature.structure.all.coding$ensembl_gene_id)))==0)
+head(bm.interpro.all.clean)
 #' Make a small version with coordinates and the most important information.
 #' Add +/-10kb coordinates.
 get_gene_table_locations<-function(gene_table){
@@ -251,12 +322,12 @@ get_gene_table_locations<-function(gene_table){
            gene_end_bp_plus_10k=gene_end_bp+10000)
   return(gene_table_locations)
 }
-gene.locations.coding<-get_gene_table_locations(bm.all.coding)
-gene.locations.noncoding<-get_gene_table_locations(bm.all.noncoding)
+gene.locations.coding<-get_gene_table_locations(bm.feature.structure.all.coding)
+gene.locations.noncoding<-get_gene_table_locations(bm.feature.structure.all.noncoding)
 
 #' Save the tables
-for (rtable in c("bm.all.coding","bm.all.noncoding",
-                 "gene.locations.coding","gene.locations.noncoding")){
+for (rtable in c("bm.feature.structure.all.coding","bm.feature.structure.all.noncoding",
+                 "gene.locations.coding","gene.locations.noncoding","bm.interpro.all.clean")){
   rtable.fname<-gsub("\\.","-",rtable)
   rtable.path<-file.path(rtables.dir.path,
                          paste(paste(format(Sys.time(),format="%Y%m%d"),
@@ -271,6 +342,7 @@ for (rtable in c("bm.all.coding","bm.all.noncoding",
               col.names = T)
 
 }
+save.image(file="./output/rdafiles/001-get-genes-go-to-ensembl-workspace.Rdata")
 
 # Final table format in the paper by Xia et al. 
 # (https://www.nature.com/articles/s41586-024-07095-8):
@@ -280,3 +352,5 @@ for (rtable in c("bm.all.coding","bm.all.noncoding",
 # Final table format in this script:
 # chromosome_name gene_name       gene_start_bp   gene_end_bp     strand  strand_new      gene_start_bp_minus_10k gene_end_bp_plus_10k
 # 13      F2r     95738311        95754995        -1      -       95728311        95764995
+
+load(file="./output/rdafiles/001-get-genes-go-to-ensembl-workspace.Rdata")
